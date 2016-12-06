1eb434210d0b89cb983962bb39f99ee76be94fb2
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-12-06 11:54:46 +0530

    Adding CD's and saving to file

--- 3. Practical: A Simple Database/practice.lisp
+++ 3. Practical: A Simple Database/practice.lisp
@@ -41,5 +41,24 @@
   (make-cd
     (prompt-read "Title")
     (prompt-read "Artist")
-    (prompt-read "Rating")
-    (prompt-read "Ripped [y/n]")))
+    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+    (y-or-n-p "Ripped [y/n]")))
+
+; asking add cd or not until the user is done
+(defun add-cds()
+  (loop (add-record (prompt-for-cd))
+        (if (not (y-or-n-p "Add another CD? [y/n]: ")) (return))))
+
+; saving to files
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+;function to load db
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
